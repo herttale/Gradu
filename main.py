36e642fbf0b17eb0c49e5170c1bff839e16410ff
Hertta Sydänlammi <hertta.lehvavirta@gmail.com>
2018-10-06 20:57:08 +0300

    build iterative district generation

--- main.py
+++ main.py
@@ -8,56 +8,66 @@ Created on Fri Jul 27 18:33:29 2018
 
 import geopandas as gpd
 import pandas as pd
+import math
 
 
 rttk = gpd.read_file("/home/hertta/Documents/Gradu/oppilasalueet2018/wrangled_rttk.shp", encoding = "UTF-8")
 schools = gpd.read_file("/home/hertta/Documents/Gradu/oppilasalueet2018/koulut_euref_ykr.shp", encoding = "UTF-8")
 
-# for testing purposes, create a new column in rttk including "pupils": 0.2 * ki_vakiy
-rttk1['pupils'] = rttk1['ki_vakiy']*0.15
-for key, row in rttk1.iterrows():
+# for testing purposes, create a new column in rttk including "pupils": 0.2 * ki_vakiy. In next versions this must be done when generating the data (rttk_wrangling)
+rttk['pupils'] = rttk['ki_vakiy']*0.15
+for key, row in rttk.iterrows():
     if row['pupils'] < 0:
-        rttk1.loc[key,'pupils'] = 0
-
-##################### testataan block -ja schooldistr 
-testb_attr = rttk1.iloc[0,:]
-testblock = Block(geometry = testb_attr['geometry'], ykrId = testb_attr['YKR_ID'] , zvalue = testb_attr['z-value'], studentBase = testb_attr['pupils'])
+        rttk.loc[key,'pupils'] = 0
 
 
-# lisätään ttmatrixit school-tauluun
-schools["ttmatrix"] = None
 
-for index, row in schools.iterrows():
-    ykrid = row['YKR_ID']
-    fourFirst = str(ykrid)[0:4]
-    
-    fp = "/home/hertta/Documents/Gradu/ttmatrices/HelsinkiTravelTimeMatrix2018/" + fourFirst + "xxx/" +"travel_times_to_ " + str(ykrid) + ".txt"
-    matrix = pd.read_csv(fp, sep = ";")
-    matrix_ind = matrix.set_index("from_id")
-    
-    # tässä pitää käsitellä -1 arvot ja poistaa turhat sarakkeet myöhemmissä versioissa
-    # lisäksi pitää miettiä, mikä dictirakenne on kätevin: https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_dict.html
-    
-    ttdict = matrix_ind.to_dict(orient = 'index')
-    schools.loc[index,"ttmatrix"] = [ttdict]
+
 
 # groupataan rttk
 rttk1 = rttk1.set_index(keys = 'YKR_ID', drop = False)
 rttk_grouped = rttk1.groupby(by = 'ID')
 
+districts = {}
+blocks_dict = {}
 
-# build testdistr
+######### build blocks, districts and separate dict of blocks
 for key, row in schools.iterrows():
-    schoolID = row['id']
     
-    ttmatrix = row['ttmatrix']
+    ##### fetch the correct travel time matrix based on school ykrID
+    ykrid = row['YKR_ID']
+    
+    # build correct filepath and read the file
+    fourFirst = str(ykrid)[0:4] 
+    fp = "/home/hertta/Documents/Gradu/ttmatrices/HelsinkiTravelTimeMatrix2018/" + fourFirst + "xxx/" +"travel_times_to_ " + str(ykrid) + ".txt"
+    matrix = pd.read_csv(fp, sep = ";")
+    
+    # set the starting location as index
+    matrix_ind = matrix.set_index("from_id", drop = False)
+    
+    # replace -1 values with infinity
+    matrix_ind = matrix_ind.replace(to_replace= -1, value = math.inf)
+    
+    # drop unnecessary columns
+    matrix_ind = matrix_ind.filter(items = ['from_id', 'to_id', 'walk_d'])
+    
+    #convert matrix to dict
+    ttdict = matrix_ind.to_dict(orient = 'index')
+    
+    # fetch the right block-group, create blocks iteratively and add them to both "blocks" and global variable blocks_dict
     blocksframe = rttk_grouped.get_group(schoolID)
     blocks = {}
     for key, row in blocksframe.iterrows():
         block = Block(geometry = row['geometry'], ykrId = row['YKR_ID'] , zvalue = row['z-value'], studentBase = row['pupils'])
         blocks[row['YKR_ID']] = block
+        blocks_dict[row['YKR_ID']] = block
     
-    testdistr = SchoolDistr(schoolID, blocks, ttmatrix)
+    # get the last attribute schoolID
+    schoolID = row['id']
+    
+    # now create district and add it to dict "districts"
+    distr = SchoolDistr(schoolID, blocks, ttdict)
+    districts[row['id']] = distr
     break
     
 
